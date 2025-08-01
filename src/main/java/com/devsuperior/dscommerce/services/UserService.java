cc412503e079a32fd83bbd3127206b875af82df2
Igor Daniel Pacheco dos Santos <igor.santos@saamauditoria.com.br>
2025-08-01 15:56:48 -0300

    Adicionado pacote customgrant e implementado o AuthorizationServerConfig

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -5,12 +5,18 @@ import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
 import com.devsuperior.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class UserService implements UserDetailsService {
 
     @Autowired
