2d1a089aa62db7f21e71eda7895b54bdf489de5c
Igor Daniel Pacheco dos Santos <igor.santos@saamauditoria.com.br>
2025-08-02 10:25:04 -0300

    Implementado o UserController

--- src/main/java/com/devsuperior/dscommerce/services/UserService.java
+++ src/main/java/com/devsuperior/dscommerce/services/UserService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dscommerce.services;
 
+import com.devsuperior.dscommerce.dto.UserDTO;
 import com.devsuperior.dscommerce.entities.Role;
 import com.devsuperior.dscommerce.entities.User;
 import com.devsuperior.dscommerce.projections.UserDetailsProjection;
@@ -12,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -36,5 +38,23 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User getAuthenticated() {
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return repository.findByEmail(username).get();
+        }
+        catch (Exception e) {
+            throw new UsernameNotFoundException("Invalid user");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        User entity = getAuthenticated();
+        return new UserDTO(entity);
     }
 }
